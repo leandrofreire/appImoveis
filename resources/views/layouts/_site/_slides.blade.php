34963a191e5c951cad4e1bca2965722651622199
leandrofreire <rafaelleandrofreire@gmail.com>
2017-05-01 18:48:18 -0300

    Criando o slider e o footer usando o materialize e o jquery

---
+++ resources/views/layouts/_site/_slides.blade.php
@@ -0,0 +1,25 @@
+<div class="slider">
+  <ul class="slides">
+    <li>
+      <img src="{{ asset('img/1.jpg') }}" alt="Imagem" />
+      <div class="caption left-align">
+        <h3>Titulo da Imagem</h3>
+        <h5>Descrição do slide</h5>
+      </div>
+    </li>
+    <li>
+      <img src="{{ asset('img/2.jpg') }}" alt="Imagem" />
+      <div class="caption left-align">
+        <h3>Titulo da Imagem</h3>
+        <h5>Descrição do slide</h5>
+      </div>
+    </li>
+    <li>
+      <img src="{{ asset('img/3.jpg') }}" alt="Imagem" />
+      <div class="caption left-align">
+        <h3>Titulo da Imagem</h3>
+        <h5>Descrição do slide</h5>
+      </div>
+    </li>
+  </ul>
+</div>
