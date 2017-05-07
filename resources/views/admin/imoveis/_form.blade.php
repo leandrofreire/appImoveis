c744b573068f62129ad03cfcf236bd16e4a2c155
leandrofreire <rafaelleandrofreire@gmail.com>
2017-05-07 14:46:23 -0300

    Views para adicionar um novo estabelecimento

--- resources/views/admin/imoveis/_form.blade.php
+++ resources/views/admin/imoveis/_form.blade.php
@@ -1,14 +1,112 @@
 <div class="input-field">
-  <input type="text" name="nome" class="validate" value="{{ isset($registro->nome)? $registro->nome : '' }}" />
-  <label>Nome da cidade</label>
+  <input type="text" name="titulo" class="validate" value="{{ isset($registro->titulo)? $registro->titulo : '' }}" />
+  <label>Título do imóvel</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="estado" class="validate" value="{{ isset($registro->estado)? $registro->estado : '' }}" />
-  <label>Estado</label>
+  <input type="text" name="descricao" class="validate" value="{{ isset($registro->descricao)? $registro->descricao : '' }}" />
+  <label>Descrição do imóvel</label>
+</div>
+
+<div class="row">
+  <div class="file-field input-field col m6 s12">
+    <div class="btn">
+      <span>Imagem</span>
+      <input type="file" name="imagem" value="">
+    </div>
+    <div class="file-path-wrapper">
+      <input type="text" class="file-path validate">
+    </div>
+  </div>
+  <div class="col m6 s12">
+    @if(isset($registro->imagem))
+      <img width="120" src="{{ asset($registro->imagem) }}" alt="" />
+    @endif
+  </div>
+</div>
+
+<div class="input-field col s12">
+  <select class="status" name="status">
+    <option value="aluga" {{(isset($registro->aluga)
+      && $registro->status == 'aluga' ? 'selected' : '')}}>Aluga
+    </option>
+    <option value="aluga" {{(isset($registro->vende)
+      && $registro->status == 'vende' ? 'selected' : '')}}>Vende
+    </option>
+  </select>
+  <label>Status</label>
+</div>
+
+<div class="input-field col s12">
+  <select name="tipo_id">
+    @foreach($tipos as $tipo)
+    <option value="{{ $tipo->id }}" {{(isset($registro->tipo_id)
+      && $registro->status == 'aluga' ? 'selected' :)}}>Aluga
+    </option>
+    <option value="aluga" {{(isset($registro->vende)
+      && $registro->tipo_id == $tipo->id ? 'selected' : '')}}>{{ $tipo->titulo }}
+    </option>
+    @endforeach
+  </select>
+  <label>Tipo</label>
+</div>
+
+<div class="input-field col s12">
+  <select name="tipo_id">
+    @foreach($cidades as $cidade)
+    <option value="{{ $cidade->id }}" {{(isset($registro->cidade_id)
+      && $registro->status == 'aluga' ? 'selected' :)}}>Aluga
+    </option>
+    <option value="aluga" {{(isset($registro->vende)
+      && $registro->cidade_id == $cidade->id ? 'selected' : '')}}>{{ $cidade->titulo }}
+    </option>
+    @endforeach
+  </select>
+  <label>Cidade</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="sigla_estado" class="validate" value="{{ isset($registro->sigla_estado)? $registro->sigla_estado : '' }}" />
-  <label>Sigla</label>
+  <input type="text" name="valor" class="validate" value="{{ isset($registro->valor)? $registro->valor : '' }}" />
+  <label>Valor</label>
 </div>
+
+<div class="input-field">
+  <input type="text" name="dormitorios" class="validate" value="{{ isset($registro->dormitorios)? $registro->dormitorios : '' }}" />
+  <label>Dormitorios</label>
+</div>
+
+<div class="input-field">
+  <input type="text" name="detaçhes" class="validate" value="{{ isset($registro->detaçhes)? $registro->detaçhes : '' }}" />
+  <label>Detalhes</label>
+</div>
+
+<div class="input-field">
+  <textarea name="mapa">
+    {{ (isset($registro->mapa) ? $registro->mapa : '') }}
+  </textarea>
+  <label>Mapa (Cole o ifame do Google)</label>
+</div>
+
+
+<div class="input-field col s12">
+  <select name="publicar">
+    <option value="nao" {{(isset($registro->publicar)
+      && $registro->publicar == 'nao' ? 'selected' : '')}}>Não
+    </option>
+    <option value="sim" {{(isset($registro->publicar)
+      && $registro->publicar == 'sim' ? 'selected' : '')}}>Sim
+    </option>
+  </select>
+  <label>Publicar ?</label>
+</div>
+
+
+
+
+
+
+
+
+
+
+//
--- resources/views/admin/imoveis/adicionar.blade.php
+++ resources/views/admin/imoveis/adicionar.blade.php
@@ -3,22 +3,22 @@
 
 @section('content')
 <div class="container">
-  <h5 class="center">Adicionar tipos</h5>
+  <h5 class="center">Adicionar imóvel</h5>
   <div class="row">
     <nav>
       <div class="nav-wrapper green">
         <div class="col s12">
           <a href="{{ route('admin.principal') }}" class="breadcrumb">Inicio</a>
-          <a href="{{ route('admin.cidades') }}" class="breadcrumb">Lista de cidades</a>
-          <a class="breadcrumb">Adicionar tipo</a>
+          <a href="{{ route('admin.imoveis') }}" class="breadcrumb">Lista de imóveis</a>
+          <a class="breadcrumb">Adicionar imóvel</a>
         </div>
       </div>
     </nav>
   </div>
   <div class="row">
-    <form class="" action="{{ route('admin.cidades.salvar') }}" method="post">
+    <form class="" action="{{ route('admin.imoveis.salvar') }}" method="post" enctype="multipart/form-data">
       {{ csrf_field() }}
-      @include('admin.cidades._form')
+      @include('admin.imoveis._form')
       <button class="btn blue">Adicionar</button>
     </form>
   </div>
