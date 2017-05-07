d943abaa0cab2b9c7002d913bd2c6ad03736d58b
leandrofreire <rafaelleandrofreire@gmail.com>
2017-05-07 15:56:01 -0300

    Finalizando o crud de imoveis

--- app/Http/Controllers/Admin/ImovelController.php
+++ app/Http/Controllers/Admin/ImovelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Imovel;
 use App\Tipo;
@@ -10,82 +12,126 @@ use App\Cidade;
 
 class ImovelController extends Controller
 {
-  public function index()
-  {
-    $registros = Imovel::all();
-    return view('admin.imoveis.index', compact('registros'));
-  }
-  public function adicionar()
-  {
-    //Mandar lista de cidade e tipos
-    $tipos = Tipo::all();
-    $cidades = Cidade::all();
-
-    return view('admin.imoveis.adicionar', compact('tipos', 'cidades'));
-  }
-  public function salvar(Request $request)
-  {
-    $dados = $request->all();
-    $registro = new Cidade();
-    $registro->nome = $dados['nome'];
-    $registro->estado = $dados['estado'];
-    $registro->sigla_estado = $dados['sigla_estado'];
-    $registro->save();
-    \Session::flash('mensagem',[
-      'msg'=> 'Registro criado com sucesso',
-      'class' => 'green white-text'
-    ]);
-    return redirect()->route('admin.cidades');
-  }
-  public function editar($id)
-  {
-    $registro = Cidade::find($id);
-    return view('admin.cidades.editar', compact('registro'));
-  }
-  public function atualizar(Request $request,$id)
-  {
-    $registro = Cidade::find($id);
-    $dados = $request->all();
-    $registro->nome = $dados['nome'];
-    $registro->estado = $dados['estado'];
-    $registro->sigla_estado = $dados['sigla_estado'];
-    $registro ->update();
-    \Session::flash('mensagem',[
-      'msg'=> 'Registro atualizado com sucesso',
-      'class' => 'green white-text'
-    ]);
-    return redirect()->route('admin.cidades');
-  }
-
-  public function deletar($id)
-  {
-    /*
-    Teste para verificar se existe imovel
-    Relacionado a essa cidade que quer deletar
-    */
-    if(Imovel::where('cidade_id','=',$id)->count()){
-
-      $msg = "Não é possível deletar essa cidade!
-      Esses imóveis(";
-      $imoveis = Imovel::where('cidade_id','=',$id)->get();
-
-      foreach ($imoveis as $imovel) {
-        $msg .= "id:".$imovel->id." ";
-      }
-      $msg.= ") estão relacionados.";
-
-      \Session::flash('mensagem',[
-        'msg'=> $msg,
-        'class' => 'red white-text'
-      ]);
-      return redirect()->route('admin.cidades');
+    public function index()
+    {
+        $registros = Imovel::all();
+        return view('admin.imoveis.index',compact('registros'));
     }
-    Cidade::find($id)->delete();
 
-    \Session::flash('mensagem',[
-      'msg'=> 'Registro deletado com sucesso',
-      'class' => 'green white-text'
-    ]);
-    return redirect()->route('admin.cidades');
-  }
+    public function adicionar()
+    {
+
+        $tipos = Tipo::all();
+        $cidades = Cidade::all();
+
+        return view('admin.imoveis.adicionar',compact('tipos','cidades'));
+    }
+
+    public function salvar(Request $request)
+    {
+        $dados = $request->all();
+
+        $registro = new Imovel();
+        $registro->titulo = $dados['titulo'];
+        $registro->descricao = $dados['descricao'];
+        $registro->status = $dados['status'];
+        $registro->status= $dados['status'];
+        $registro->endereco= $dados['endereco'];
+        $registro->cep= $dados['cep'];
+        $registro->valor= $dados['valor'];
+        $registro->dormitorios= $dados['dormitorios'];
+        $registro->detalhes= $dados['detalhes'];
+        $registro->visualizacoes= 0;
+        $registro->publicar= $dados['publicar'];
+        if(isset($dados['mapa']) && trim($dados['mapa']) != "" ){
+            $registro->mapa = trim($dados['mapa']);
+        }else{
+            $registro->mapa = null;
+        }
+
+        $registro->cidade_id= $dados['cidade_id'];
+        $registro->tipo_id= $dados['tipo_id'];
+
+        $file = $request->file('imagem');
+    	if($file){
+    		$rand = rand(11111,99999);
+    		$diretorio = "img/imoveis/".str_slug($dados['titulo'],'_')."/";
+    		$ext = $file->guessClientExtension();
+    		$nomeArquivo = "_img_".$rand.".".$ext;
+    		$file->move($diretorio,$nomeArquivo);
+    		$registro->imagem = $diretorio.'/'.$nomeArquivo;
+    	}
+
+
+        $registro->save();
+
+        \Session::flash('mensagem',['msg'=>'Registro criado com sucesso!','class'=>'green white-text']);
+
+        return redirect()->route('admin.imoveis');
+
+    }
+
+    public function editar($id)
+    {
+        $registro = Imovel::find($id);
+
+        $tipos = Tipo::all();
+        $cidades = Cidade::all();
+
+        return view('admin.imoveis.editar',compact('registro','tipos','cidades'));
+
+    }
+
+    public function atualizar(Request $request, $id)
+    {
+        $registro = Imovel::find($id);
+        $dados = $request->all();
+
+        $registro->titulo = $dados['titulo'];
+        $registro->descricao = $dados['descricao'];
+        $registro->status = $dados['status'];
+        $registro->status= $dados['status'];
+        $registro->endereco= $dados['endereco'];
+        $registro->cep= $dados['cep'];
+        $registro->valor= $dados['valor'];
+        $registro->dormitorios= $dados['dormitorios'];
+        $registro->detalhes= $dados['detalhes'];
+
+        $registro->publicar= $dados['publicar'];
+        if(isset($dados['mapa']) && trim($dados['mapa']) != "" ){
+            $registro->mapa = trim($dados['mapa']);
+        }else{
+            $registro->mapa = null;
+        }
+
+        $registro->cidade_id= $dados['cidade_id'];
+        $registro->tipo_id= $dados['tipo_id'];
+
+        $file = $request->file('imagem');
+    	if($file){
+    		$rand = rand(11111,99999);
+    		$diretorio = "img/imoveis/".str_slug($dados['titulo'],'_')."/";
+    		$ext = $file->guessClientExtension();
+    		$nomeArquivo = "_img_".$rand.".".$ext;
+    		$file->move($diretorio,$nomeArquivo);
+    		$registro->imagem = $diretorio.'/'.$nomeArquivo;
+    	}
+
+
+        $registro ->update();
+
+        \Session::flash('mensagem',['msg'=>'Registro atualizado com sucesso!','class'=>'green white-text']);
+
+        return redirect()->route('admin.imoveis');
+    }
+
+    public function deletar($id)
+    {
+
+        Imovel::find($id)->delete();
+
+        \Session::flash('mensagem',['msg'=>'Registro deletado com sucesso!','class'=>'green white-text']);
+        return redirect()->route('admin.imoveis');
+
+    }
 }
