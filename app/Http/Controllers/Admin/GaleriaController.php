ac97002f117f20619c23628dd8cd57d339896f5e
leandrofreire <rafaelleandrofreire@gmail.com>
2017-05-07 19:16:58 -0300

    CRUD galeria - Editar e deletar galerias

--- app/Http/Controllers/Admin/GaleriaController.php
+++ app/Http/Controllers/Admin/GaleriaController.php
@@ -60,65 +60,49 @@ class GaleriaController extends Controller
 
   public function editar($id)
   {
-      $registro = Imovel::find($id);
-
-      $tipos = Tipo::all();
-      $cidades = Cidade::all();
-
-      return view('admin.imoveis.editar',compact('registro','tipos','cidades'));
+      $registro = Galeria::find($id);
+      $imovel = $registro->imovel;
+      return view('admin.galerias.editar',compact('registro','imovel'));
 
   }
 
   public function atualizar(Request $request, $id)
   {
-      $registro = Imovel::find($id);
+      $registro = Galeria::find($id);
       $dados = $request->all();
 
       $registro->titulo = $dados['titulo'];
       $registro->descricao = $dados['descricao'];
-      $registro->status = $dados['status'];
-      $registro->status= $dados['status'];
-      $registro->endereco= $dados['endereco'];
-      $registro->cep= $dados['cep'];
-      $registro->valor= $dados['valor'];
-      $registro->dormitorios= $dados['dormitorios'];
-      $registro->detalhes= $dados['detalhes'];
+      $registro->ordem = $dados['ordem'];
 
-      $registro->publicar= $dados['publicar'];
-      if(isset($dados['mapa']) && trim($dados['mapa']) != "" ){
-          $registro->mapa = trim($dados['mapa']);
-      }else{
-          $registro->mapa = null;
-      }
-
-      $registro->cidade_id= $dados['cidade_id'];
-      $registro->tipo_id= $dados['tipo_id'];
+      $imovel = $registro->imovel;
 
       $file = $request->file('imagem');
-    if($file){
+      if($file){
       $rand = rand(11111,99999);
-      $diretorio = "img/imoveis/".str_slug($dados['titulo'],'_')."/";
+      $diretorio = "img/imoveis/".str_slug($imovel->titulo,'_')."/";
       $ext = $file->guessClientExtension();
       $nomeArquivo = "_img_".$rand.".".$ext;
       $file->move($diretorio,$nomeArquivo);
       $registro->imagem = $diretorio.'/'.$nomeArquivo;
     }
 
-
       $registro ->update();
 
       \Session::flash('mensagem',['msg'=>'Registro atualizado com sucesso!','class'=>'green white-text']);
 
-      return redirect()->route('admin.imoveis');
+      return redirect()->route('admin.galerias',$imovel->id);
   }
 
   public function deletar($id)
   {
 
-      Imovel::find($id)->delete();
+      $galeria = Galeria::find($id);
+      $imovel = $galeria->imovel;
+      $galeria->delete();
 
       \Session::flash('mensagem',['msg'=>'Registro deletado com sucesso!','class'=>'green white-text']);
-      return redirect()->route('admin.imoveis');
+      return redirect()->route('admin.galerias',$imovel->id);
 
   }
 }
