cbb45088fb2cfe24188d5f88431a9ac9e9bb7dbd
leandrofreire <rafaelleandrofreire@gmail.com>
2017-05-07 16:52:34 -0300

    Model e migration das imagens dos detalhes

--- app/Imovel.php
+++ app/Imovel.php
@@ -9,17 +9,22 @@ class Imovel extends Model
     // Define o nome da tabela
     protected $table = "imoveis";
 
-    //Relacionamento das tabelas 1 pra 1
+    //Relacionamento das tabelas muitos pra 1
     public function tipo()
     {
                       //Id da tabela pra relacionar
       return $this->belongsTo('App\Tipo','tipo_id');
     }
 
-    //Relacionamento das tabelas 1 pra muitos
+    //Relacionamento das tabelas muitos pra 1
     public function cidade()
     {
                       //Id da tabela pra relacionar
       return $this->belongsTo('App\Cidade','cidade_id');
+    }
+    // Relacionamento das tabelas 1 pra muitos
+    public function galeria()
+    {
+      return $this->hasMany('App\Galeria', 'imovel_id');
     }
 }
