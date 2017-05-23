a3e1aeeb351cfda71b7aa8192d2cb025295e1bb2
leandrofreire <rafaelleandrofreire@gmail.com>
2017-05-22 22:08:51 -0300

    Sistema de filtro no laravelv

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -6,14 +6,30 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Imovel;
 use App\Slide;
+use App\Tipo;
+use App\Cidade;
 
 class HomeController extends Controller
 {
     public function index()
     {
-      $imoveis = Imovel::orderBy('id','desc')->paginate(20);
+      $imoveis = Imovel::where('publicar','=','sim')->orderBy('id','desc')->paginate(20);
       $slides = Slide::where('publicado','=','sim')->orderBy('ordem')->get();
       $direcaoImagem = ['center-align', 'left-align', 'right-align'];
-      return view('site.home', compact('imoveis','slides','direcaoImagem'));
+      $paginacao = true;
+      $tipos = Tipo::orderBy('titulo')->get();
+      $cidades = Cidade::orderBy('nome')->get();
+      return view('site.home', compact('imoveis','slides','direcaoImagem','paginacao','tipos','cidades'));
+    }
+    //Método de busca (filtro)
+    public function busca(Request $request){
+      $busca = $request->all();
+      $imoveis = Imovel::where('publicar','=','sim')->orderBy('id','desc')->get();
+      $paginacao = false;
+      $tipos = Tipo::orderBy('titulo')->get();
+      $cidades = Cidade::orderBy('nome')->get();
+
+      dd($busca);
+      return view('site.busca', compact('busca', 'imoveis','paginacao','tipos','cidades'));
     }
 }
