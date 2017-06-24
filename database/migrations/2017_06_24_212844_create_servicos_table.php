668c55fc97c6de4c55cfc363a06cbb5e9f123b92
leandrofreire <rafaelleandrofreire@gmail.com>
2017-06-24 20:24:01 -0300

    Migration de servicos

---
+++ database/migrations/2017_06_24_212844_create_servicos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('oficina_id')->unsigned();
+            $table->foreign('oficina_id')->references('id')->on('oficinas');
+            $table->string('titulo');
+            $table->decimal('valor',6,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicos');
+    }
+}
