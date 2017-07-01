4ac67e63e5dc0b967a82fd1a8e361ec0a012e4c3
leandrofreire <rafaelleandrofreire@gmail.com>
2017-07-01 15:39:21 -0300

    Criando as migracoes de permissoes

---
+++ database/migrations/2017_07_01_153646_create_papels_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePapelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('papels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->timestamps();
+        });
+                        // O nome relciona os models
+        Schema::create('papel_permissao', function (Blueprint $table) {
+            // N para N
+            $table->integer('permissao_id')->unsigned();
+            $table->integer('papel_id')->unsigned();
+
+            $table->foreign('permissao_id')->references('id')
+            ->on('permissaos')->onDelete('cascade');
+
+            $table->foreign('papel_id')->references('id')
+            ->on('papels')->onDelete('cascade');
+
+            $table->primary(['permissao_id','papel_id']);
+
+        });
+
+        Schema::create('papel_user', function (Blueprint $table) {
+            // N para N
+            $table->integer('user_id')->unsigned();
+            $table->integer('papel_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')
+            ->on('users');
+
+            $table->foreign('papel_id')->references('id')
+            ->on('papels');
+
+            $table->primary(['user_id','papel_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('papels');
+        Schema::drop('papel_user');
+        Schema::drop('papel_permissao');
+    }
+}
