a962d794744e76c82fb3a8ed0ffdfea2397396ed
leandrofreire <rafaelleandrofreire@gmail.com>
2017-07-01 16:19:37 -0300

    Criando os models de permissao

--- app/Papel.php
+++ app/Papel.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Papel extends Model
 {
-    //
+  protected $fillable = [
+    'nome',
+    'descricao'
+  ];
+
+  public function permissoes()
+  {
+    return $this->belongsToMany(Permissao::class);
+  }
 }
--- app/Permissao.php
+++ app/Permissao.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permissao extends Model
 {
-    //
+    protected $fillable = [
+      'nome',
+      'descricao'
+    ];
+
+    public function papeis()
+    {
+      return $this->belongsToMany(Papel::class);
+    }
 }
