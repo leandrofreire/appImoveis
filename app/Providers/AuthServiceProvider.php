1f8a9b5f9316820a530ba54ff2b818bfb487a63e
leandrofreire <rafaelleandrofreire@gmail.com>
2017-07-02 15:19:01 -0300

    Definindo as permissoes no sistema com Seeders

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+      /*  Gate::define( 'listar-usuarios',
+        function($user,$permissao){
+          return true == $permissao;
+        });*/
     }
 }
